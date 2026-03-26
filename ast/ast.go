7c8fde8176353ce1713b80afceffbbf4406793a1
Michael Zhan <michaelzhan2001@gmail.com>
2026-03-26 00:48:17 -0400

    basic parsing

---
+++ ast/ast.go
@@ -0,0 +1,55 @@
+package ast
+
+import "github.com/michaelzhan1/go-interpreter/token"
+
+// Node is the underlying interface for all nodes in an AST
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement is a Node that does not produce a value
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression is a Node that produces a value
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is the root node of an AST
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// Identifier is an expression node that represents a token.IDENT token
+type Identifier struct {
+	Token token.Token // token.IDENT
+}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+func (i *Identifier) expressionNode() {}
+
+// LetStatement is a statement node that represents a token.LET token
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+func (ls *LetStatement) statementNode() {}
---
+++ parser/parser.go
@@ -0,0 +1,121 @@
+package parser
+
+import (
+	"fmt"
+
+	"github.com/michaelzhan1/go-interpreter/ast"
+	"github.com/michaelzhan1/go-interpreter/lexer"
+	"github.com/michaelzhan1/go-interpreter/token"
+)
+
+// Parser is the parser for the monkey language
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+
+	errors []string
+}
+
+// New returns a new Parser
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
+
+	// prime curToken and peekToken
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+// Errors returns the errors in the parser
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+// peekError adds an error associate with an unexpected token type while peeking
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+
+// nextToken moves the token forward
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+// ParseProgram parses the tokens and constructs a Program
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{
+		Statements: []ast.Statement{},
+	}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+// parseStatement parses a statement starting from a token
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+// parseLetStatement parses a let statement
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeekAndAdvance(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken}
+
+	if !p.expectPeekAndAdvance(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: take in the actual assigned value later
+
+	if !p.expectPeekAndAdvance(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+// curTokenIs checks that the parser's curToken is of a given type
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+// peekTokenIs checks that the parser's peekToken is of a given type
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+// expectPeekAndAdvance checks the parser's peekToken and advances
+func (p *Parser) expectPeekAndAdvance(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	} else {
+		p.peekError(t)
+		return false
+	}
+}
