bd204011c457920cc960886b395221f748987d0a
Michael Zhan <michaelzhan2001@gmail.com>
2026-04-04 18:12:23 -0400

    update repl

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/michaelzhan1/go-interpreter/evaluator"
 	"github.com/michaelzhan1/go-interpreter/lexer"
 	"github.com/michaelzhan1/go-interpreter/parser"
 )
@@ -32,8 +33,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
